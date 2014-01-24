4c042f0346ed843452e5275fad22a2de8fe2d027
Oded Sharon <git@odedsharon.com>
2014-01-24 13:20:43 +0200

    added ngui and a few Assets , cube moving

---
+++ Assets/Scripts/RandomMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMove : MonoBehaviour {
+
+	public Vector3 moveDirection;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		ChooseRandomDirection ();
+	
+	}
+
+
+	void ChooseRandomDirection() {
+		moveDirection = new Vector3 (Random.Range (0f, Mathf.PI), Random.Range (0f, Mathf.PI), 0);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController> ();
+
+		controller.Move(moveDirection * moveSpeed *  Time.deltaTime);
+
+	}
+}
